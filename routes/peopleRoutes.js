f2d1174fa50ec28bc7b27b5eb997404a9cb5d562
Gaston Sagasti <gastisagasti@gmail.com>
2024-09-27 23:08:18 -0300

    Termino el domingo!

--- routes/peopleRoutes.js
+++ routes/peopleRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const peopleController = require('../controllers/peopleController');
 
+
+
+router.get('/filtered', peopleController.listFilteredPeopleByDepartment);
+
 router.get('/', peopleController.listPeople);
 
 router.get('/:id', peopleController.getPersonById);
-
-//router.get('/top_rated', peopleController.listTopRatedSeries);
-
 
 module.exports = router;
